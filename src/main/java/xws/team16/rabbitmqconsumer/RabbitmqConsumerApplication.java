7e987beca6b28100dd344ace5bd393539093a54b
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-06-28 19:12:00 +0200

    Initial commit

---
+++ src/main/java/xws/team16/rabbitmqconsumer/RabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package xws.team16.rabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqConsumerApplication.class, args);
+	}
+
+}
