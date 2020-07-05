585bdecb23aa41a6e7b3a164590ef4dcb1b6433b
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-07-05 21:06:51 +0200

    Added connection to heroku cloudAMQP

--- src/main/java/xws/team16/rabbitmqconsumer/configuration/MailConfiguration.java
+++ src/main/java/xws/team16/rabbitmqconsumer/configuration/MailConfiguration.java
@@ -1,6 +1,8 @@
 package xws.team16.rabbitmqconsumer.configuration;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,16 @@ public class MailConfiguration {
     @Bean
     public Queue queue(){
         return new Queue(QUEUE, false);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setUsername("falysqic");
+        connectionFactory.setPassword("WGXFaLcXLNsampQVJGT8FO-6nXQne-zh");
+        connectionFactory.setVirtualHost("falysqic");
+        connectionFactory.setHost("wasp.rmq.cloudamqp.com");
+//        connectionFactory.setPort();
+        return connectionFactory;
     }
 }
